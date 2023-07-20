4ba9e1d47659cae09f028f2e4c53c2e72d47767d
Francesca Baronissi <84421636+freyja92@users.noreply.github.com>
2023-07-20 20:44:10 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,80 @@
+//create a web server
+
+const http = require('http');
+const port = 3000;
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+server.listen(port, function(){
+    console.log('Server running at http://localhost:${port}/');
+});
+control + enter
+//open browser and type localhost:3000
+//you will see Hello World in the browser
+
+// Path: comments.js
+//create a web server
+const http = require('http');
+const port = 3000;
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+server.listen(port, function(){
+    console.log('Server running at http://localhost:${port}/');
+});
+control + enter
+//open browser and type localhost:3000
+//you will see Hello World in the browser
+
+// Path: comments.js
+//create a web server
+const http = require('http');
+const port = 3000;
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+server.listen(port, function(){
+    console.log('Server running at http://localhost:${port}/');
+});
+control + enter
+//open browser and type localhost:3000
+//you will see Hello World in the browser
+
+// Path: comments.js
+//create a web server
+const http = require('http');
+const port = 3000;
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+server.listen(port, function(){
+    console.log('Server running at http://localhost:${port}/');
+});
+control + enter
+//open browser and type localhost:3000
+//you will see Hello World in the browser
+
+// Path: comments.js
+//create a web server
+const http = require('http');
+const port = 3000;
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+server.listen(port, function(){
+    console.log('Server running at http://localhost:${port}/');
+});
+control + enter
+//open browser and type localhost:3000
+
+
